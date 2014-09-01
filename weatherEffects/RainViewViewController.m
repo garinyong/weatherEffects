50ed61c3e800ebe94be497dd41a4fb8841ef1fd9
garin <garinspring@163.com>
2014-09-01 11:48:36 +0800

    ok

--- weatherEffects/RainViewViewController.m
+++ weatherEffects/RainViewViewController.m
@@ -28,43 +28,38 @@
     [super viewDidLoad];
     
     self.view.backgroundColor = [UIColor colorWithRed:58/255.0 green:99/255.0 blue:132/255.0 alpha:1];
-    // Do any additional setup after loading the view.
-    // Configure the particle emitter to the top edge of the screen
-	CAEmitterLayer *snowEmitter = [CAEmitterLayer layer];
-	snowEmitter.emitterPosition = CGPointMake(self.view.bounds.size.width / 2.0, -30);
-	snowEmitter.emitterSize		= CGSizeMake(self.view.bounds.size.width * 2.0, 0.0);;
+
+    [self addTaste];
+}
+
+-(void) addTaste
+{
+    //发射层
+    CAEmitterLayer *rainEmitter = [CAEmitterLayer layer];
+	rainEmitter.emitterPosition = CGPointMake(self.view.bounds.size.width / 2.0, -30);//发射源位置
+	rainEmitter.emitterSize		= CGSizeMake(self.view.bounds.size.width * 2.0, 0.0);//发射尺寸大小
 	
-	// Spawn points for the flakes are within on the outline of the line
-	snowEmitter.emitterMode		= kCAEmitterLayerOutline;
-	snowEmitter.emitterShape	= kCAEmitterLayerLine;
+	rainEmitter.emitterMode		= kCAEmitterLayerOutline;  //发射模式
+	rainEmitter.emitterShape	= kCAEmitterLayerLine;     //发射形状
 	
-	// Configure the snowflake emitter cell
-	CAEmitterCell *snowflake = [CAEmitterCell emitterCell];
-	
-	snowflake.birthRate		= 80;
-	snowflake.lifetime		= 40.0;
-    snowflake.scale = 0.1;
+    //粒子
+	CAEmitterCell *rainCell = [CAEmitterCell emitterCell];
+    rainCell.contents		= (id) [[UIImage imageNamed:@"CUSSenderRain"] CGImage];
+	rainCell.color			= [[UIColor colorWithRed:112/255.0 green:148/255.0 blue:176/255.0 alpha:1] CGColor];
     
-    snowflake.scaleSpeed = 0.01;
-	
-	snowflake.velocity		= 70;				// falling down slowly
-//	snowflake.velocityRange = 5;
-	snowflake.yAcceleration = 60;
-//	snowflake.emissionRange = 0.5 * M_PI;		// some variation in angle
-//	snowflake.spinRange		= 0.25 * M_PI;		// slow spin
-	
-	snowflake.contents		= (id) [[UIImage imageNamed:@"yudi"] CGImage];
-	snowflake.color			= [[UIColor colorWithRed:112/255.0 green:148/255.0 blue:176/255.0 alpha:1] CGColor];
+    rainCell.birthRate		= 40;    //每s 发射的粒子个数
+	rainCell.lifetime		= 8.0;   //每个粒子显示的时间
+    rainCell.lifetimeRange  = 2;     //变化幅度
+    rainCell.scale = 0.13;
+	rainCell.velocity		= -1000; //速度
     
-	// Make the flakes seem inset in the background
-	snowEmitter.shadowOpacity = 1.0;
-	snowEmitter.shadowRadius  = 0.0;
-	snowEmitter.shadowOffset  = CGSizeMake(0.0, 1.0);
-	snowEmitter.shadowColor   = [[UIColor whiteColor] CGColor];
+	rainEmitter.shadowOpacity = 1.0;
+	rainEmitter.shadowRadius  = 0.0;
+	rainEmitter.shadowOffset  = CGSizeMake(0.0, 1.0);
+	rainEmitter.shadowColor   = [[UIColor whiteColor] CGColor];
 	
-	// Add everything to our backing layer below the UIContol defined in the storyboard
-	snowEmitter.emitterCells = [NSArray arrayWithObject:snowflake];
-	[self.view.layer insertSublayer:snowEmitter atIndex:0];
+	rainEmitter.emitterCells = [NSArray arrayWithObject:rainCell];
+	[self.view.layer insertSublayer:rainEmitter atIndex:0];
 }
 
 - (void)didReceiveMemoryWarning
